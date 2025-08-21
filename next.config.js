2fb00decbc0496a2e0b3fadefa5d5d01909c564b
lansehuiyi6 <lansehuiyi6@163.com>
2025-08-21 15:26:26 +0800

    Revert "just try"
    This reverts commit fbb7d0823ca5b143264d5c983d63a772d45ae8d6.

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Enable server components external packages
-  experimental: {
-    serverComponentsExternalPackages: ['axios', 'technicalindicators']
-  },
-  // Use webpack to bundle server dependencies
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals = [];
-    }
-    return config;
-  },
-  // Ensure all dependencies are bundled in serverless functions
-  output: 'standalone'
-};
+const nextConfig = {};
+
+// module.exports = nextConfig;
 
 export default nextConfig;
